aa327b0e47233133c80397090e2ab5cdc20939d0
Vasiliy <tabaqui.vn@gmail.com>
2020-02-14 10:18:20 +0300

    adding all owners list. Closes #14

--- sfg-pet-clinic-web/src/main/kotlin/ru/crew/motley/sfgpetclinic/controllers/OwnerController.kt
+++ sfg-pet-clinic-web/src/main/kotlin/ru/crew/motley/sfgpetclinic/controllers/OwnerController.kt
@@ -1,13 +1,18 @@
 package ru.crew.motley.sfgpetclinic.controllers
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
+import ru.crew.motley.sfgpetclinic.services.OwnerService
 
 @Controller
-class OwnerController {
+class OwnerController(private val ownerService: OwnerService) {
 
-    @RequestMapping(path = ["/owners", "/owners/index", "/owners/index.html"])
-    fun listOwners(): String {
+    @RequestMapping(path = ["", "/owners", "/owners/index", "/owners/index.html"])
+    fun listOwners(model: Model): String {
+
+        model.addAttribute("owners", ownerService.findAll())
+
         return "owners/index"
     }
 }
