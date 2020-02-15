972981ae60a6acce353e8e4c8497fd6d62ebb40a
Vasiliy <tabaqui.vn@gmail.com>
2020-02-15 18:02:37 +0300

    add contact properties to Owner. Closes #25

--- sfg-pet-clinic-data/src/main/kotlin/ru/crew/motley/sfgpetclinic/model/Owner.kt
+++ sfg-pet-clinic-data/src/main/kotlin/ru/crew/motley/sfgpetclinic/model/Owner.kt
@@ -3,5 +3,8 @@ package ru.crew.motley.sfgpetclinic.model
 class Owner(
         firstName: String,
         lastName: String,
+        var address: String,
+        var city: String,
+        var telephone: String,
         var pets: Set<Pet>)
     : Person(firstName, lastName)
