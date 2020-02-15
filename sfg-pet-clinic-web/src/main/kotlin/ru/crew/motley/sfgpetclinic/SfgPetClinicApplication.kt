725ac6127728c10e726aee43701a3ce300a9d661
Vasiliy <tabaqui.vn@gmail.com>
2020-02-15 15:03:55 +0300

    Cleaning

--- sfg-pet-clinic-web/src/main/kotlin/ru/crew/motley/sfgpetclinic/SfgPetClinicApplication.kt
+++ sfg-pet-clinic-web/src/main/kotlin/ru/crew/motley/sfgpetclinic/SfgPetClinicApplication.kt
@@ -3,23 +3,11 @@ package ru.crew.motley.sfgpetclinic
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
 
 
 @SpringBootApplication(exclude = [GsonAutoConfiguration::class])
 //@SpringBootApplication(scanBasePackages = ["ru.crew.motley.sfgpetclinic"])
-class SfgPetClinicApplication {
-    @Controller
-    internal class IndexController {
-        @GetMapping("/")
-        fun index(model: Model): String {
-            model.addAttribute("greeting", "Hello, Wro4j!")
-            return "index"
-        }
-    }
-}
+class SfgPetClinicApplication
 
 fun main(args: Array<String>) {
     runApplication<SfgPetClinicApplication>(*args)
