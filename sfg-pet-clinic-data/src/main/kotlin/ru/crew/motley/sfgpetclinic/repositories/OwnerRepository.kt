a6fd5d31e04962fb6328e79add98b84ec4574bd5
Vasiliy <tabaqui.vn@gmail.com>
2020-02-17 11:16:06 +0300

    added JPA repositories. Closes #36

---
+++ sfg-pet-clinic-data/src/main/kotlin/ru/crew/motley/sfgpetclinic/repositories/OwnerRepository.kt
@@ -0,0 +1,8 @@
+package ru.crew.motley.sfgpetclinic.repositories
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import ru.crew.motley.sfgpetclinic.model.Owner
+
+@Repository
+interface OwnerRepository : CrudRepository<Owner, Long>
