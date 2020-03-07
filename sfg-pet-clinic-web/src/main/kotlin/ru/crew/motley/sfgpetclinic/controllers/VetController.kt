bededacb7d016eb9324c5f76b05e5f935b0e4113
Vasiliy <tabaqui.vn@gmail.com>
2020-03-07 20:14:46 +0300

    Added Vets JSON Endpoint. Closes #65

--- sfg-pet-clinic-web/src/main/kotlin/ru/crew/motley/sfgpetclinic/controllers/VetController.kt
+++ sfg-pet-clinic-web/src/main/kotlin/ru/crew/motley/sfgpetclinic/controllers/VetController.kt
@@ -2,7 +2,10 @@ package ru.crew.motley.sfgpetclinic.controllers
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import ru.crew.motley.sfgpetclinic.model.Vet
 import ru.crew.motley.sfgpetclinic.services.VetService
 
 @Controller
@@ -14,5 +17,11 @@ class VetController(private val vetsService: VetService) {
         model.addAttribute("vets", vetsService.findAll())
 
         return "vets/index"
+    }
+
+    @GetMapping("/api/vets")
+    @ResponseBody
+    fun getVetsJson(): Set<Vet> {
+        return vetsService.findAll()
     }
 }
